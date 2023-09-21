119c0f98b7c44a8b19c6b975d5d3518368688f3d
@matheusdoedev <matheusdoe.dev@gmail.com>
2023-09-21 09:44:02 -0300

    feat: new python exercises

---
+++ python/bee1000.py
@@ -0,0 +1,3 @@
+# Problem link: https://www.beecrowd.com.br/judge/en/problems/view/1000
+
+print("Hello World!")
---
+++ python/bee1001.py
@@ -0,0 +1,8 @@
+# Problem link: https://www.beecrowd.com.br/judge/en/problems/view/1001
+
+a = int(input()) 
+b = int(input())
+
+x = a + b
+
+print("X = " + str(x))
